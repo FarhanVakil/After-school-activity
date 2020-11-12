1a5ceab4631045a35aa7577fc9cc1af270168e1b
Farhan Vakil <aneefari@yahoo.com>
2020-11-12 16:20:50 +0000

    Added lessons.js

---
+++ lessons.js
@@ -0,0 +1,48 @@
+let lessons= [
+    {
+        id:1,
+        name: 'Football',
+        image:'football.jpg',
+        location:'Chelsea',
+        price:150,
+        availability:10
+    
+    },
+
+    {
+        id:2,
+        name: 'Cricket',
+        image:'cricket.jpg',
+        location:'Harrow',
+        price:200,
+        availability:10
+    },
+
+    {
+        id:3,
+        name: 'Swimming',
+        image:'swimming.jpg',
+        location:'Acton Town',
+        price:150,
+        availability:10
+    },
+    
+    {
+        id:4,
+        name: 'Rugby',
+        image:'rugby.jpg',
+        location:'Watford',
+        price:150,
+        availability:10
+    },
+
+    {
+        id:5,
+        name: 'Snooker',
+        image:'snooker.jpg',
+        location:'Wembley Park',
+        price:50,
+        availability:5
+    }
+
+];
